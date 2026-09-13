0abcb37c1743c9d3f787d52aee55cab1846d508e
Chris Ndungu <128899227+Chris-Ndungu@users.noreply.github.com>
2026-09-13 14:45:19 +0300

    Add IMF and FX marts

--- dbt/models/marts/analytics/mart_country_profile.sql
+++ dbt/models/marts/analytics/mart_country_profile.sql
@@ -2,6 +2,17 @@ with facts as (
     select * from {{ ref('fact_macroeconomic_indicators') }}
 ),
 
+latest_fx as (
+    select
+        country_code,
+        fx_rate_usd as latest_fx_rate_usd
+    from {{ ref('fact_fx_rates') }}
+    qualify row_number() over (
+        partition by country_code
+        order by ingestion_date desc, loaded_at desc
+    ) = 1
+),
+
 countries as (
     select * from {{ref('dim_country')}}
 ), 
@@ -24,8 +35,10 @@ select
     i.metric_unit,
     
     -- Metrics
-    f.indicator_value
+    f.indicator_value,
+    fx.latest_fx_rate_usd
 
 from facts f 
 left join countries c on f.country_code = c.country_code
 left join indicators i on f.indicator_code = i.indicator_code
+left join latest_fx fx on f.country_code = fx.country_code
--- dbt/models/marts/dimensions/dim_country.sql
+++ dbt/models/marts/dimensions/dim_country.sql
@@ -1,5 +1,23 @@
 {{ config(materialized = 'table') }}
-select distinct
-    country_code, -- Primary Key
-    country_name
-from {{ ref('stg_world_bank__indicators') }}
+
+with country_ids as (
+    select country_code from {{ ref('stg_world_bank__indicators') }}
+    union distinct
+    select country_id as country_code from {{ ref('stg_imf__indicators') }}
+    union distinct
+    select country_id as country_code from {{ ref('stg_fx_rates__daily_rates') }}
+),
+
+country_names as (
+    select
+        country_code,
+        max(country_name) as country_name
+    from {{ ref('stg_world_bank__indicators') }}
+    group by country_code
+)
+
+select
+    ids.country_code,
+    names.country_name
+from country_ids as ids
+left join country_names as names using (country_code)
